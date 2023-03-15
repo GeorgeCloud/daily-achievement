717becad5ba527d55b7a8977600a1eec76ec1973
Ahyeon Jeon <ahyeonjeon@Ahyeons-MBP.attlocal.net>
2023-03-15 14:08:37 -0700

    fix: resolved conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{html,js}"],
+  content: [
+    "./pages/**/*.{js,}",
+  ],
   theme: {
     extend: {},
   },
